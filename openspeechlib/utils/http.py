e5b201163887a1dd903e510817b5e4e8bc41a01a
ma0c <ma0@contraslash.com>
2020-04-12 00:45:43 -0500

    finally having scripts to extract and filter all resources, closes #11

--- openspeechlib/utils/http.py
+++ openspeechlib/utils/http.py
@@ -3,7 +3,13 @@ Utility functions for HTTP
 
 This will be used as proxy in case we don't want to rely on requests in the future
 """
-
+from urllib import parse
 import requests
 
 get = requests.get
+
+HTTPError = requests.exceptions.HTTPError
+
+def extract_query_param_from_url_string(url, param):
+    parsed_url = parse.urlparse(url)
+    return parse.parse_qs(parsed_url.query).get(param)
