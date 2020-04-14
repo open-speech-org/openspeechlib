2ebc86ad786f82b5a5ce580a4a69a51af725c9fd
ma0c <ma0@contraslash.com>
2020-04-13 20:42:10 -0500

    finishing MFCC fix #3

--- openspeechlib/feature_extraction/mfcc.py
+++ openspeechlib/feature_extraction/mfcc.py
@@ -1,13 +1,17 @@
 import numpy as np
 
 from openspeechlib.feature_extraction.utils.filters import pre_emphasis
+from openspeechlib.feature_extraction.utils.windows import extract_overlapping_frames_from_signal, hamming
+from openspeechlib.feature_extraction.utils.mel_scale import transform_hz_signal_into_mel_scale_using_triangular_filter_banks
+from openspeechlib.feature_extraction.utils.delta import delta
 
 
 def MFCC(
         signal,
         frequency=16000,
-        window_width=16000*0.025,
-        window_offset=16000*0.01
+        window_width=int(16000*0.025),
+        window_offset=int(16000*0.01),
+        number_of_mel_filters=40,
 ):
     """
     This functions calculates the Mel Frequency Cepstral Coeffients of a signal using the following process
@@ -25,3 +29,25 @@ def MFCC(
     :return:
     """
     signal_with_pre_emphasis = pre_emphasis(signal)
+    frames = extract_overlapping_frames_from_signal(signal_with_pre_emphasis, window_width, window_offset)
+    hamming_frame = hamming(window_width)
+    windowed_frames = np.apply_along_axis(lambda x: x*hamming_frame, 1, frames)
+    windowed_frames_with_fourier_transform = np.fft.fft(windowed_frames)
+    energy = np.sum(np.square(windowed_frames), axis=1)
+
+    mel_frames = transform_hz_signal_into_mel_scale_using_triangular_filter_banks(
+        windowed_frames_with_fourier_transform,
+        frequency,
+        number_of_mel_filters
+    )
+
+    mel_frames_without_zeros = np.where(mel_frames == 0, np.finfo(float).eps, mel_frames)
+
+    log_of_mel_frames = np.log(mel_frames_without_zeros)
+    inverse_fourier_of_log_frames = np.fft.ifft(log_of_mel_frames)
+
+    first_delta = np.apply_along_axis(delta, 0, inverse_fourier_of_log_frames)
+    second_delta = np.apply_along_axis(delta, 0, first_delta)
+
+    return np.concatenate((inverse_fourier_of_log_frames, first_delta, second_delta, energy[np.newaxis].T), axis=1)
+
