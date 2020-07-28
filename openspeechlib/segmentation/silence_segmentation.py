a1728113d51a67ecadb73405bf42b176e049ecbe
Mauricio Collazos <ma0acollazos@gmail.com>
2020-07-27 23:44:54 -0500

    new silence segmentator

--- openspeechlib/segmentation/silence_segmentation.py
+++ openspeechlib/segmentation/silence_segmentation.py
@@ -6,15 +6,19 @@ from openspeechlib.utils.windows import extract_overlapping_frames_from_signal, 
 
 
 def default_segmentator(procesed_signal, threshold, **kwargs):
+    frequency = int(kwargs.get("frequency", 16000))
     voice_ranges = np.where(procesed_signal > np.max(procesed_signal) * threshold, 1, 0)
     deltas = delta(voice_ranges, (1, -1))
     changes_to_positive = np.where(deltas > 0)
     changes_to_negative = np.where(deltas < 0)
-    return zip(changes_to_positive[0], changes_to_negative[0])
+    return zip(changes_to_positive[0]*frequency, changes_to_negative[0]*frequency)
 
 
 def skip_adjacent_segmentator(procesed_signal, threshold, **kwargs):
-    skip_adjacent = int(kwargs.get("skip_adjacent", 0))
+    skip_adjacent = int(kwargs.get("skip_adjacent", 10))
+    min_silence_gap = float(kwargs.get("min_silence_gap", 0.5))
+    windows_offset_size = float(kwargs.get("windows_offset_size", 0.01))
+    frequency = int(kwargs.get("frequency", 16000))
     silences_start = list()
     signal_start = list()
     is_silence = True
@@ -22,35 +26,40 @@ def skip_adjacent_segmentator(procesed_signal, threshold, **kwargs):
     skipped_segments = 0
     possible_start = 0
     print(threshold_value)
-    for index, element in enumerate(procesed_signal):
+    index = 0
+    max_iterations = len(procesed_signal) * 5
+    iteration = 0
+    while index < len(procesed_signal) and iteration < max_iterations:
+        element = procesed_signal[index]
+        index += 1
+        max_iterations += 1
         if is_silence and element > threshold_value:
             if skipped_segments == 0:
-                print("Possible start for signal", index)
                 possible_start = index
             skipped_segments += 1
             if skipped_segments > skip_adjacent:
-                print(index, element, is_silence, element > threshold_value)
                 is_silence = False
                 signal_start.append(possible_start)
-                possible_start = index
+                index = possible_start
                 skipped_segments = 0
-                print("1")
-
                 continue
         if not is_silence and element < threshold_value:
             if skipped_segments == 0:
-                print("Possible start for silence", index)
                 possible_start = index
             skipped_segments += 1
             if skipped_segments > skip_adjacent:
-                print(index, element, is_silence, element > threshold_value)
                 is_silence = True
                 silences_start.append(possible_start)
-                possible_start = index
+                index = possible_start
                 skipped_segments = 0
-                print("2")
-
-    return zip(silences_start, signal_start)
+    silence_gap_size = min_silence_gap / windows_offset_size
+    silence_gaps = list()
+    for index in range(len(signal_start) - 1):
+        if signal_start[index + 1] - silences_start[index] > silence_gap_size:
+            silence_gaps.append((
+                silences_start[index] * frequency * windows_offset_size,
+                signal_start[index + 1] * frequency * windows_offset_size))
+    return silence_gaps
 
 
 def silence_segmentation(
@@ -58,12 +67,14 @@ def silence_segmentation(
         frequency=16000,
         threshold=0.03,
         window_width_size=0.025,
-        windows_offset=0.01,
+        windows_offset_size=0.01,
         segmentator=default_segmentator,
         extra_segmentator_args=None
 ):
+    if extra_segmentator_args is None:
+        extra_segmentator_args = dict()
     window_width = int(frequency*window_width_size)
-    windows_offset = int(frequency*windows_offset)
+    windows_offset = int(frequency*windows_offset_size)
     frames = extract_overlapping_frames_from_signal(
         signal,
         window_width,
@@ -71,6 +82,19 @@ def silence_segmentation(
     )
     windowed_frames = apply_window_function_to_frames(frames)
     energy = power(windowed_frames, axis=1)
-    frame_start_time = np.arange(0, signal.shape[-1], windows_offset)
-    interpolated_values = np.interp(np.arange(0, signal.shape[0]), frame_start_time, energy, )
-    return segmentator(interpolated_values, threshold)
+    # I think I don't need interpolate
+    # Instead I can multiply the values of the energy with the step size
+    # frame_start_time = np.arange(0, signal.shape[-1], windows_offset)
+    # interpolated_values = np.interp(np.arange(0, signal.shape[0]), frame_start_time, energy, )
+    return segmentator(
+        energy,
+        threshold,
+        **{
+            **extra_segmentator_args,
+            **{
+                "frequency": frequency,
+                "window_width_size": window_width_size,
+                "windows_offset_size": windows_offset_size,
+            }
+        }
+    )
